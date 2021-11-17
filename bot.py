2d2151524ffdae6d4104c0b95c1cd8c87771b28e
thanh.tran <thanh.tran@wearefram.com>
2021-11-17 21:34:43 +0700

    loop buy token when have error

--- bot.py
+++ bot.py
@@ -9,6 +9,10 @@ from web3 import Web3
 import json
 import config
 import time
+import datetime
+# import asyncio
+# from pythonpancakes import PancakeSwapAPI
+
 
 # Binance smart chain mainnet.
 # Notice:
@@ -23,7 +27,7 @@ sender_address = ''
 
 # Address of token to buy
 # busd: 0xe9e7cea3dedca5984780bafc599bd69add087d56
-tokenToBuyAddress = ''
+tokenToBuyAddress = '0xe9e7cea3dedca5984780bafc599bd69add087d56'
 bnbAmountToBuy = 0.0001
 
 
@@ -65,29 +69,87 @@ tokenToBuy = web3.toChecksumAddress(tokenToBuyAddress)
 
 spend = web3.toChecksumAddress(wbnbAddress)
 
+# contract = web3.eth.contract(address=panRouterContractAddress, abi=panabi)
 contract = web3.eth.contract(address=panRouterContractAddress, abi=panabi)
+
 
 # transaction detail
 nonce = web3.eth.get_transaction_count(sender_address)
 
 start = time.time()
 
-pancakeswap2_txn = contract.functions.swapExactETHForTokens(
-    0,
-    [spend, tokenToBuy],
-    sender_address,
-    (int(time.time()) + 1000)
-).buildTransaction({
-    'from': sender_address,
-    'value': web3.toWei(bnbAmountToBuy, 'ether'),
-    'gasPrice': web3.toWei(gasFeeWei, 'gwei'),
-    'nonce': nonce,
-})
 
-signed_txn = web3.eth.account.sign_transaction(
-    pancakeswap2_txn, private_key=senderPrivateKey)
-tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+# buy
+def buyToken():
+    pancakeswap2_txn = contract.functions.swapExactETHForTokens(
+        0,
+        [spend, tokenToBuy],
+        sender_address,
+        (int(time.time()) + 1000)
+    ).buildTransaction({
+        'from': sender_address,
+        'value': web3.toWei(bnbAmountToBuy, 'ether'),
+        'gasPrice': web3.toWei(gasFeeWei, 'gwei'),
+        'nonce': nonce,
+    })
+    # print('pancakeswap2_txn', pancakeswap2_txn)
 
-print('Result Tx:', web3.toHex(tx_token))
-print('You can check your result here: ',
-      'https://bscscan.com/tx/' + web3.toHex(tx_token))
+    signed_txn = web3.eth.account.sign_transaction(
+        pancakeswap2_txn, private_key=senderPrivateKey)
+    tx_token = web3.eth.send_raw_transaction(signed_txn.rawTransaction)
+
+    print('Result Tx:', web3.toHex(tx_token))
+    print('You can check your result here: ',
+          'https://bscscan.com/tx/' + web3.toHex(tx_token))
+
+
+def loopBuy():
+    try:
+        buyToken()
+    except Exception as e:
+        print(datetime.datetime.now(), 'Error when buy, auto try again: ', e)
+        loopBuy()
+
+
+loopBuy()
+
+
+############################
+# # track new pair
+# def handle_event(event):
+#     print(Web3.toJSON(event))
+
+# # asynchronous defined function to loop
+# # this loop sets up an event filter and is looking for new entires for the "PairCreated" event
+# # this loop runs on a poll interval
+
+
+# async def log_loop(event_filter, poll_interval):
+#     while True:
+#         for PairCreated in event_filter.get_new_entries():
+#             handle_event(PairCreated)
+#         await asyncio.sleep(poll_interval)
+
+
+# # when main is called
+# # create a filter for the latest block and look for the "PairCreated" event for the uniswap factory contract
+# # run an async loop
+# # try to run the log_loop function above every 2 seconds
+# def main():
+#     event_filter = contract.events.PairCreated.createFilter(fromBlock='latest')
+#     print(event_filter)
+#     #block_filter = web3.eth.filter('latest')
+#     # tx_filter = web3.eth.filter('pending')
+#     loop = asyncio.get_event_loop()
+#     try:
+#         loop.run_until_complete(
+#             asyncio.gather(
+#                 log_loop(event_filter, 2)))
+#         # log_loop(block_filter, 2),
+#         # log_loop(tx_filter, 2)))
+#     finally:
+#         # close loop to free up system resources
+#         loop.close()
+
+
+# main()
---
+++ uniswap.py
@@ -0,0 +1,55 @@
+# import the following dependencies
+import json
+from web3 import Web3
+import asyncio
+
+# add your blockchain connection information
+infura_url = 'https://bsc-dataseed.binance.org/'
+web3 = Web3(Web3.HTTPProvider(infura_url))
+
+# uniswap address and abi
+uniswap_router = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+uniswap_factory = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+uniswap_factory_abi = json.loads('[{"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"token0","type":"address"},{"indexed":true,"internalType":"address","name":"token1","type":"address"},{"indexed":false,"internalType":"address","name":"pair","type":"address"},{"indexed":false,"internalType":"uint256","name":"","type":"uint256"}],"name":"PairCreated","type":"event"},{"constant":true,"inputs":[{"internalType":"uint256","name":"","type":"uint256"}],"name":"allPairs","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"allPairsLength","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"tokenA","type":"address"},{"internalType":"address","name":"tokenB","type":"address"}],"name":"createPair","outputs":[{"internalType":"address","name":"pair","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"feeTo","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"feeToSetter","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"internalType":"address","name":"","type":"address"},{"internalType":"address","name":"","type":"address"}],"name":"getPair","outputs":[{"internalType":"address","name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeTo","type":"address"}],"name":"setFeeTo","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"internalType":"address","name":"_feeToSetter","type":"address"}],"name":"setFeeToSetter","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"}]')
+
+contract = web3.eth.contract(address=uniswap_factory, abi=uniswap_factory_abi)
+
+
+# define function to handle events and print to the console
+def handle_event(event):
+    print(Web3.toJSON(event))
+    # and whatever
+
+
+# asynchronous defined function to loop
+# this loop sets up an event filter and is looking for new entires for the "PairCreated" event
+# this loop runs on a poll interval
+async def log_loop(event_filter, poll_interval):
+    while True:
+        for PairCreated in event_filter.get_new_entries():
+            handle_event(PairCreated)
+        await asyncio.sleep(poll_interval)
+
+
+# when main is called
+# create a filter for the latest block and look for the "PairCreated" event for the uniswap factory contract
+# run an async loop
+# try to run the log_loop function above every 2 seconds
+def main():
+    event_filter = contract.events.PairCreated.createFilter(fromBlock='latest')
+    #block_filter = web3.eth.filter('latest')
+    # tx_filter = web3.eth.filter('pending')
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(
+            asyncio.gather(
+                log_loop(event_filter, 2)))
+                # log_loop(block_filter, 2),
+                # log_loop(tx_filter, 2)))
+    finally:
+        # close loop to free up system resources
+        loop.close()
+
+
+
+main()
